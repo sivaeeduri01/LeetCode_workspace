8a4ce3985a87474de5672b07b0cb0bd70057a10e
Siva_Eeduri <shiva.workspace1@gmail.com>
2026-06-26 09:44:54 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,7 @@
+num=int(input(" Enter a number:"))
+if (num  == 0 ):
+     print (num ,"is invalid.")
+elif(num % 2 == 0):
+     print (num ,"is an even number.")
+else:
+    print (num ,"is a odd number .")
